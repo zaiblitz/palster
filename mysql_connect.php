c17058b41b5e19088b96cfe261f07fbb55b35d02
Merwin <merwin@tripleonetech.com>
2017-06-19 18:55:23 +0800

    test

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,16 +1,15 @@
 <?php
 
 
-echo 'ok';exit;
 
-$dbhost = 'palster';
+$dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '3a1403197c4445f6567802cae59d5b37e0631de04245d2f1';
 $dbname = 'zcon';
 
 $conn = mysql_connect($dbhost,$dbuser,$dbpass);
 
+print_r($conn);exit;
 
-
-mysql_select_db($dbname);
+// mysql_select_db($dbname);
 ?>
