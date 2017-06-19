d92af0eb262f23b85df903c51b2484799d7a9647
Merwin <merwin@tripleonetech.com>
2017-06-19 18:38:09 +0800

    update database

--- mysql_connect.php
+++ mysql_connect.php
@@ -3,7 +3,7 @@
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '3a1403197c4445f6567802cae59d5b37e0631de04245d2f1';
-$dbname = 'mydatabase';
+$dbname = 'zcon';
 
 $conn = mysql_connect($dbhost,$dbuser,$dbpass);
 
