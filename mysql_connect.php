e00f4cfb8cd2d9b2f567404e59c31d632ac61321
Merwin <merwin@tripleonetech.com>
2017-06-19 18:57:24 +0800

    test mysqli

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,15 +1,9 @@
 <?php
+$con = mysqli_connect("localhost","root","3a1403197c4445f6567802cae59d5b37e0631de04245d2f1","zcon");
 
-
-
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '3a1403197c4445f6567802cae59d5b37e0631de04245d2f1';
-$dbname = 'zcon';
-
-$conn = mysql_connect($dbhost,$dbuser,$dbpass);
-
-print_r($conn);exit;
-
-// mysql_select_db($dbname);
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>
