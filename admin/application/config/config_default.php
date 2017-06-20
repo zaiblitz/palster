c96c663d9e2a039bb3d459004744a4a3ddfaa869
Merwin <merwin@tripleonetech.com>
2017-06-20 12:43:41 +0800

    ci config default

--- admin/application/config/config_default.php
+++ admin/application/config/config_default.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://zai.local/palster/palster/z-admin/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
