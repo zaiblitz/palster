4a023ed19f5f32a844785300637b01222448c085
Merwin <merwin@tripleonetech.com>
2017-06-20 12:36:29 +0800

    config management

---
+++ admin/application/controllers/config_management.php
@@ -0,0 +1,14 @@
+<?php
+
+class Config_Management extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->model('admin_user');
+    }
+
+    public function index() {
+        echo 'ok';
+    }
+}
