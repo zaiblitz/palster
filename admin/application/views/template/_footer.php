7d27e9b4d30577d05570abc89d2f2d4f42a15661
Merwin <merwin@tripleonetech.com>
2017-06-23 20:53:47 +0800

    Add template

---
+++ admin/application/views/template/_footer.php
@@ -0,0 +1,55 @@
+
+<!-- footer content -->
+<footer>
+    <div class="pull-right">
+        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+
+<!-- jQuery -->
+<script src="<?= $resources; ?>jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?= $resources; ?>bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="<?= $resources; ?>fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="<?= $resources; ?>nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="<?= $resources; ?>Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="<?= $resources; ?>gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="<?= $resources; ?>bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="<?= $resources; ?>iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="<?= $resources; ?>skycons/skycons.js"></script>
+<!-- Flot -->
+<script src="<?= $resources; ?>Flot/jquery.flot.js"></script>
+<script src="<?= $resources; ?>Flot/jquery.flot.pie.js"></script>
+<script src="<?= $resources; ?>Flot/jquery.flot.time.js"></script>
+<script src="<?= $resources; ?>Flot/jquery.flot.stack.js"></script>
+<script src="<?= $resources; ?>Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="<?= $resources; ?>flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="<?= $resources; ?>flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="<?= $resources; ?>flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="<?= $resources; ?>DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="<?= $resources; ?>jqvmap/dist/jquery.vmap.js"></script>
+<script src="<?= $resources; ?>jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="<?= $resources; ?>jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="<?= $resources; ?>moment/min/moment.min.js"></script>
+<script src="<?= $resources; ?>bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="<?= $resources_common; ?>js/custom.min.js"></script>
+
+</body>
+</html>
