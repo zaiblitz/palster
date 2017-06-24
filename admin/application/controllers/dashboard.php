ff55dc5d580f1eba86c5b011fa4fd1606f633ffc
Merwin <merwin@tripleonetech.com>
2017-06-24 17:10:25 +0800

    update

--- admin/application/controllers/dashboard.php
+++ admin/application/controllers/dashboard.php
@@ -1,20 +1,9 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-    public function __construct() {
-        parent::__construct();
-
-        $this->load->model('admin_user');
-    }
-
     public function index() {
-
         $this->load->view('dashboard/index');
     }
-
-    public function get_data() {
-        echo 'ok';
-    }
-
 }
