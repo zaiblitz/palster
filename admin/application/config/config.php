e915a1d89bc5bc36954e198e9bd7b9e7fdb9f9b7
zaiblitz <merwindale.domingo@gmail.com>
2017-06-26 10:36:13 +0000

    remove

--- admin/application/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-// load CI config default first
-require_once dirname(__FILE__) . '/config_default.php';
-
-if (file_exists(dirname(__FILE__) . '/../../../secret_keys/config_secret_local.php')) {
-    require_once dirname(__FILE__) . '/../../../secret_keys/config_secret_local.php';
-}
