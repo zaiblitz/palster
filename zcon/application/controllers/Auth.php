47815886be59ef6a31cdad959f3625903da346b6
zaiblitz <merwindale.domingo@gmail.com>
2017-06-26 10:55:29 +0000

    ZCon

---
+++ zcon/application/controllers/Auth.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function index()
+	{
+	  echo 'Under Development';
+	 //$this->load->view('welcome_message');
+	}
+}
