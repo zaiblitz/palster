5c4b794bdb44ea1bf4765502e905ebdf4e7b332a
Merwin <merwin@tripleonetech.com>
2017-06-30 21:04:16 +0800

    add list

--- zcon/application/views/dashboard/list.php
+++ zcon/application/views/dashboard/list.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ZConnect</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>resources/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>resources/font-awesome-4.2.0/css/font-awesome.min.css">
 </head>
 <body>
-<h4>Hello, world!</h4>
+
+<div class="container-fluid">
+    Hello <i class="fa fa-cloud-download" aria-hidden="true"></i>
+</div>
 
 <script src="<?php echo base_url(); ?>resources/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="<?php echo base_url(); ?>resources/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
