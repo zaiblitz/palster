cab80ae899b5eddc377944fee5fed7f0b75a1b7f
Merwin <merwin@tripleonetech.com>
2017-06-30 20:29:24 +0800

    add dashboard view

---
+++ zcon/application/views/dashboard/list.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ZConnect</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>resources/bower_components/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+<h4>Hello, world!</h4>
+
+<script src="<?php echo base_url(); ?>resources/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>resources/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
