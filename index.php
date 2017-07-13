c52a6a706c5b628cb78e5733b985fc7905727478
Merwin <merwin@tripleonetech.com>
2017-07-13 18:36:30 +0800

    update contact section

--- index.php
+++ index.php
@@ -50,8 +50,6 @@
                 <p>
                     I have 4+ years of general experience in the web-development industry.
 
-                    I'm highly professional and communicative, always deliver on time and Guarantee 100% Satisfaction and Quality.
-
                     I can develop a simple web site or complex web application with or without help of
                     UX/front-end developers. Mainly I am focused on back-end programming but ready to manage the whole application building process when needed.
                 </p>
@@ -424,15 +422,12 @@
         <div class="intro">
             <img class="profile-image" src="images/zai-profile.gif" alt="">
             <div class="dialog">
-                <p>I'm currently taking on freelance work. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.
-                    Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-                    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+                <p> I'm highly professional and communicative, always deliver on time and Guarantee 100% Satisfaction and Quality.</p>
                 <p><strong>I can help with the following:</strong></p>
                 <ul class="list-unstyled service-list">
                     <li><i class="fa fa-check" aria-hidden="true"></i> App development with ReactJS</li>
                     <li><i class="fa fa-check" aria-hidden="true"></i> Front-end development with AngularJS</li>
-                    <li><i class="fa fa-check" aria-hidden="true"></i> Back-end development with Django/Python</li>
+                    <li><i class="fa fa-check" aria-hidden="true"></i> Back-end development with PHP</li>
                     <li><i class="fa fa-check" aria-hidden="true"></i> UI development</li>
                     <li><i class="fa fa-check" aria-hidden="true"></i> UX prototyping</li>
                 </ul>
